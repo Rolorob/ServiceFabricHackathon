86e6c20212fcd72214d1b693a9d5343bc99d1961
Mark van Schaik <mark.vanschaik@infosupport.com>
2017-02-17 10:52:31 +0000

    Added opzet ReadingSplitterActor

---
+++ Common/Model/SplitReading.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
+
+namespace Common.Model
+{
+    public class SplitReading
+    {
+        public SplitReading(DateTime minute, decimal reading)
+        {
+            Minute = minute;
+            Reading = reading;
+        }
+
+        [JsonConstructor]
+        private SplitReading()
+        {
+        }
+
+        [JsonProperty]
+        [DataMember]
+        public DateTime Minute { get; private set; }
+
+        [JsonProperty]
+        [DataMember]
+        public decimal Reading { get; private set; }
+    }
+}
