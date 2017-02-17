136ecd848c3acfc34e5a933d6f83941c68dc7e06
Casper <casper.cramer@gmail.com>
2017-02-17 14:38:56 +0100

    PartitionKeyResolver

---
+++ Common/Utils/PartitionKeyResolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Common.Utils
+{
+    public static class PartitionKeyResolver
+    {
+        public static long ToPartitionKey(string value)
+        {
+            var md5 = MD5.Create();
+            var bytes = md5.ComputeHash(Encoding.ASCII.GetBytes(value));
+            return BitConverter.ToInt64(bytes, 0);
+        }
+    }
+}
