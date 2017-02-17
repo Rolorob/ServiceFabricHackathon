a8d9fc5881d9ffaa73c7411f477e1edae590e2d7
Bas Poldervaart <baspoldervaart92@hotmail.com>
2017-02-17 15:10:12 +0100

    Interface for AggregatedReadingService

---
+++ Common/Model/AggregatedReading.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.Serialization;
+
+namespace Common.Model
+{
+    [DataContract]
+    public class AggregatedReading
+    {
+        public AggregatedReading(DateTime startDate, DateTime endDate, decimal reading)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Reading = reading;
+        }
+
+        [JsonConstructor]
+        private AggregatedReading()
+        {
+        }
+
+        [JsonProperty]
+        [DataMember]
+        public Uri ChannelId { get; private set; }
+
+        [JsonProperty]
+        [DataMember]
+        public DateTime StartDate { get; private set; }
+
+        [JsonProperty]
+        [DataMember]
+        public DateTime EndDate { get; private set; }
+
+        [JsonProperty]
+        [DataMember]
+        public decimal Reading { get; private set; }
+    }
+}
