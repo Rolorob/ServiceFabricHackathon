10becf1a209da12fe05741f6ff844a87afd1ff61
Mark van Schaik <mark.vanschaik@infosupport.com>
2017-02-17 07:35:02 +0000

    Added DiffCalculatorActor

---
+++ Common/Model/ReadingDiff.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Model
+{
+    public class ReadingDiff
+    {
+        public ReadingDiff(
+            DateTime startTime,
+            DateTime endTime,
+            decimal diffValue)
+
+            //Uri channelId,
+            //string description,
+            //string unit,
+            //IDictionary<string, object> properties)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            DiffValue = diffValue;
+            //ChannelId = channelId;
+            //Description = description;
+            //Unit = unit;
+            //Properties = properties;
+        }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal DiffValue { get; set; }
+    }
+}
