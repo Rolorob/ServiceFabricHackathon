b22610d41e162285d56749bbf249abc3be4dde62
Mark van Schaik <mark.vanschaik@infosupport.com>
2017-02-17 07:39:07 +0000

    Edited ReadingDiff Model

--- Common/Model/ReadingDiff.cs
+++ Common/Model/ReadingDiff.cs
@@ -1,34 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Common.Model
 {
+    [DataContract]
     public class ReadingDiff
     {
         public ReadingDiff(
             DateTime startTime,
             DateTime endTime,
             decimal diffValue)
-
-            //Uri channelId,
-            //string description,
-            //string unit,
-            //IDictionary<string, object> properties)
         {
             StartTime = startTime;
             EndTime = endTime;
             DiffValue = diffValue;
-            //ChannelId = channelId;
-            //Description = description;
-            //Unit = unit;
-            //Properties = properties;
         }
 
+        [JsonConstructor]
+        private ReadingDiff()
+        {
+        }
+
+        [JsonProperty]
+        [DataMember]
         public DateTime StartTime { get; set; }
+
+        [JsonProperty]
+        [DataMember]
         public DateTime EndTime { get; set; }
+
+        [JsonProperty]
+        [DataMember]
         public decimal DiffValue { get; set; }
     }
 }
