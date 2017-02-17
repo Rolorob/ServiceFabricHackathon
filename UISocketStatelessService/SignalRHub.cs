dbf285d11bc11d1e452012ca62f67e609e80ed15
snijhof <sjoerdnijhof@hotmail.com>
2017-02-17 11:52:27 +0100

    Add SignalR

---
+++ UISocketStatelessService/SignalRHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UISocketStatelessService
+{
+    public class SignalRHub : Hub
+    {
+        public void SendData(string data)
+        {
+            //Clients.All.broadcastMessage(data);
+        }
+
+        public void RequestSubscription(string deviceId, string channel)
+        {
+            // Subscribe to the right actor
+        }
+    }
+}
